273a58ce9e6e5256a6e8fa51259d4111c5a2d386
Rajeev Kumar <143427304+iamrajeev2005@users.noreply.github.com>
2023-12-19 20:47:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const button = document.querySelector('.button')
 const notifies = document.querySelector('.notifies') 
 
 
-const randomMessage= ["Hello Shubham","Kya haal hai", "Sunai ni dera kya","Abe sun le", "Kaha Mar gya"] 
+const randomMessage= ["Hello 1", "Random 2", "Someone 3", "Anything 4", "Anything 5"] 
 
 function getNotif(){
     const notifi = document.createElement('div')
